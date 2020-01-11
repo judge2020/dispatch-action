2a82e5c219b4be47dc964966c6b6af2b24c656b4
Hunter <8601934+judge2020@users.noreply.github.com>
2020-01-11 11:24:47 -0500

    Update

--- src/download.ts
+++ src/download.ts
@@ -1,41 +1,50 @@
-let tempDirectory = process.env['RUNNER_TEMPDIRECTORY'] || '';
-import * as tc from '@actions/tool-cache';
-import * as io from '@actions/io';
-import * as path from 'path';
+let tempDirectory = process.env['RUNNER_TEMPDIRECTORY'] || ''
+import * as core from '@actions/core'
+import * as tc from '@actions/tool-cache'
+import * as io from '@actions/io'
+import * as path from 'path'
 
 if (!tempDirectory) {
-    let baseLocation;
+    let baseLocation
     if (process.platform === 'win32') {
         // On windows use the USERPROFILE env variable
-        baseLocation = process.env['USERPROFILE'] || 'C:\\';
+        baseLocation = process.env['USERPROFILE'] || 'C:\\'
     } else {
         if (process.platform === 'darwin') {
-            baseLocation = '/Users';
+            baseLocation = '/Users'
         } else {
-            baseLocation = '/home';
+            baseLocation = '/home'
         }
     }
-    tempDirectory = path.join(baseLocation, 'actions', 'temp');
+    tempDirectory = path.join(baseLocation, 'actions', 'temp')
 }
 
 export async function download(): Promise<boolean> {
-    let path: string;
+    let path: string
     path = tc.find('dispatch', '1')
-    if(!path) {
+    if (!path) {
         switch (process.platform) {
             case 'win32':
-                path = await tc.downloadTool("https://dl-dispatch.discordapp.net/download/win64");
-                break;
+                path = await tc.downloadTool(
+                    'https://dl-dispatch.discordapp.net/download/win64'
+                )
+                break
             case 'darwin':
-                path = await tc.downloadTool("https://dl-dispatch.discordapp.net/download/macos");
-                break;
+                path = await tc.downloadTool(
+                    'https://dl-dispatch.discordapp.net/download/macos'
+                )
+                break
             case 'linux':
-                path = await tc.downloadTool("https://dl-dispatch.discordapp.net/download/linux");
-                break;
+                path = await tc.downloadTool(
+                    'https://dl-dispatch.discordapp.net/download/linux'
+                )
+                break
             default:
-                throw `Error: process.platform was ${process.platform}, not one of win32, darwin, linux`;
+                throw new Error(
+                    `Error: process.platform was ${process.platform}, not one of win32, darwin, linux`
+                )
         }
     }
-    console.log(path)
-    return Promise.resolve(true);
+    core.info(path)
+    return Promise.resolve(true)
 }
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,17 @@
 import * as core from '@actions/core'
-import { download } from './download'
+import {download} from './download'
 import * as os from 'os'
 
 async function run(): Promise<void> {
     try {
         switch (os.platform()) {
             case 'win32':
-
         }
         const appid: string = core.getInput('application-id')
         core.debug(`Application ID ${appid}`)
         const token: string = core.getInput('bot-token')
         core.debug(`Bot token length ${token.length}`)
-        await download();
-
+        await download()
     } catch (error) {
         core.setFailed(error.message)
     }
